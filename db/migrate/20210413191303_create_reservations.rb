502bb7af2fdd955e73f2a0f908aede6235976b4a
Lupita <lupitarivera8899@gmail.com>
2021-04-13 17:09:59 -0500

    commit tree d0649566e951d6b4cc61764f07e3a997b3539a82

---
+++ db/migrate/20210413191303_create_reservations.rb
@@ -0,0 +1,13 @@
+class CreateReservations < ActiveRecord::Migration[5.0]
+  def change
+    create_table :reservations do |t|
+      t.date :checkin
+      t.date :checkout
+      t.integer :listing_id
+      t.integer :guest_id
+      t.string :status, default: "pending"
+
+      t.timestamps null: false
+    end
+  end
+end
